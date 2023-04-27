7348a13670a04b244d1778e8a3ebb86d9935b4bd
Daniel Castro Martin <109592821+DCM91@users.noreply.github.com>
2023-04-27 20:04:28 +0200

    cambio de idiomas en layout

--- portfoliodcm91/components/Layout.js
+++ portfoliodcm91/components/Layout.js
@@ -1,18 +1,35 @@
 import Head from 'next/head'
-import React from 'react'
 import { Footer } from './Footer'
 import { Navbar } from './navbar'
+import { useRouter } from "next/router"
+import en from "@/languages/en"
+import es from "@/languages/es"
+import Link from "next/link"
+
 
 export const Layout = ({theme, title = "Dcm91Portfolio" , children}) => {
+  const router = useRouter()
+  const t = router.locale == "en" ? en : es
   return (
-    <div>
+    <div data-theme={theme}>
         <Head>
             <title>{title}</title>
             <meta name="description" content="Dcm91 profesional Portfolio" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="icon" href="/favicon.ico" />
         </Head>
-        <header><Navbar theme={theme}/></header>
+        <header>
+          <Navbar theme={theme}/>
+          <ul>
+            {router.locales.map((loc)=>(
+                <li key={loc}>
+                    <Link href={router.asPath} locale={loc}>
+                        {loc}
+                    </Link>
+                </li>
+            ))}
+        </ul>
+        </header>
         <main>{children}</main>
         <footer><Footer theme={theme} /></footer>
     </div>
--- portfoliodcm91/components/navbar.js
+++ portfoliodcm91/components/navbar.js
@@ -1,23 +1,9 @@
-import { useRouter } from "next/router"
-import en from "@/languages/en"
-import es from "@/languages/es"
-import Link from "next/link"
 
 export const Navbar = ({theme}) => {
-    const router = useRouter()
-    const t = router.locale == "en" ? en : es
 
   return (
     <div data-theme={theme}>
-        <ul>
-            {router.locales.map((loc)=>(
-                <li key={loc}>
-                    <Link href={router.asPath} locale={loc}>
-                        {loc}
-                    </Link>
-                </li>
-            ))}
-        </ul>
+        <h1>navbar</h1>
     </div>
   )
 }
