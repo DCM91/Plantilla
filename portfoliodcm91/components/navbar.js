2f658f7b78f2dab2cef780642111c1bbb17eb9c1
Daniel Castro Martin <109592821+DCM91@users.noreply.github.com>
2023-05-03 21:35:07 +0200

    Navbar

--- portfoliodcm91/components/navbar.js
+++ portfoliodcm91/components/navbar.js
@@ -4,22 +4,24 @@ export const Navbar = ({theme}) => {
   return (
       <div data-theme={theme} className="navbar bg-base-100">
         <div className="flex-1">
-          <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+          <a className="btn btn-ghost normal-case text-xl">DCM91</a>
         </div>
         <div className="flex-none">
           <ul className="menu menu-horizontal px-1">
-            <li><a>Item 1</a></li>
+            <li><a>HOME</a></li>
             <li tabIndex={0}>
               <a>
-                Parent
+                MYSELF
                 <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
               </a>
               <ul className="p-2 bg-base-100">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
+                <li><a>ABOUT ME</a></li>
+                <li><a>SKILLS</a></li>
+                <li><a>HOBBIES</a></li>
+                <li><a>MY WORKS</a></li>
               </ul>
             </li>
-            <li><a>Item 3</a></li>
+            <li><a>CONTACT</a></li>
           </ul>
       </div>
     </div>
